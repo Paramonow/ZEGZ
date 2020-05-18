6a827ba1af5ed95c5856cd217a09794531a649cb
Paramonow <62514298+Paramonow@users.noreply.github.com>
2020-05-18 08:29:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require ("discord.js");
 const client = new discord.Client(); 
 const ytdl = require('ytdl-core');
 
-const BOT_TOKEN = "Njg4MzkzMTA2NzAxMjg3NDM1.XsIrQQ.104Hc-iJKz2fATAcdvqf4cvRheg";
+const token = "Njg4MzkzMTA2NzAxMjg3NDM1.XsIrQQ.104Hc-iJKz2fATAcdvqf4cvRheg";
 client.on ("ready", () => {
     console.log ("Gotowy");
     console.log (`${client.user.tag} wbija na discord`) 
